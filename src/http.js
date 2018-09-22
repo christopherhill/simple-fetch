132f49da5eb59c648d083eba6fc422aab502d735
Christopher Hill <accounts@christopherhill.com>
2018-09-22 11:53:23 -0700

    Initial commit.

---
+++ src/http.js
@@ -0,0 +1,75 @@
+export default class Http {
+	
+	constructor(endpoint, logging = false) {
+		this.endpoint = endpoint || window.location.hostname;
+		this.headers = new Headers({ 'Content-Type': 'application/json' });
+		this.defaultOptions = {
+			headers: this.headers,
+			mode: 'cors',
+			cache: 'default'
+		}
+		this.wrapCancelable = this.wrapCancelable.bind(this);
+	}
+
+	static wrapCancelable(url, options) {
+		const controller = new AbortController();
+		const { signal } = controller;
+		const mergedOptions = this.mergeOptions(Object.assign({}, options, { signal }));
+		let result = fetch(url, mergedOptions);
+		const cancel = () => controller.abort();
+		return {
+			result,
+			cancel
+		};
+	}
+
+	mergeOptions(additionalObject) {
+		return Object.assign({}, this.defaultOptions, additionalObject);
+	}
+
+	get(url, options) {
+		const { result, cancel } = 
+			this.wrapCancelable(`${this.endpoint}url`, { method: 'GET', ...options });
+		return {
+			exec: result.then(response => response.json()),
+			cancel,
+		}
+	}
+
+	post(url, body, options) {
+		const { result, cancel } =
+			this.wrapCancelable(`${this.endpoint}url`, { method: 'POST', body: JSON.stringify(body), ...options })
+		return {
+			exec: result.then(response => response.json()),
+			cancel,
+		}
+	}
+
+	patch(url, body, options) {
+		const { result, cancel } =
+			this.wrapCancelable(`${this.endpoint}url`, { method: 'PATCH', body: JSON.stringify(body), ...options })
+		return {
+			exec: result.then(response => response.json()),
+			cancel,
+		}
+	}
+
+	put(url, body, options) {
+		const { result, cancel } =
+			this.wrapCancelable(`${this.endpoint}url`, { method: 'PUT', body: JSON.stringify(body), ...options })
+		return {
+			exec: result.then(response => response.json()),
+			cancel,
+		}
+	}
+
+	del(url, options, signal) {
+		const { result, cancel } =
+			this.wrapCancelable(`${this.endpoint}url`, { method: 'DELETE', body: JSON.stringify(body), ...options  })
+		return {
+			exec: result.then(response => response.json()),
+			cancel,
+		}
+	}
+
+}
